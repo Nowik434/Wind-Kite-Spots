40db4684a414171842d03806ff5f4262aee96a1c
Nowik434 <nowik434@gmail.com>
2022-12-05 20:18:50 +0100

    trying to understand socket io

--- config/plugins.js
+++ config/plugins.js
@@ -8,15 +8,37 @@ module.exports = ({ env }) => ({
   },
   email: {
     config: {
-      provider: 'sendgrid',
+      provider: "sendgrid",
       providerOptions: {
-        apiKey: env('SENDGRID_API_KEY'),
+        apiKey: env("SENDGRID_API_KEY"),
       },
       settings: {
         defaultFrom: "pawel.sluzbowy@interia.pl",
         defaultReplyTo: "pawel.sluzbowy@interia.pl",
-        testAddress: 'pawel.sluzbowy@interia.pl',
+        testAddress: "pawel.sluzbowy@interia.pl",
       },
+    },
+  },
+  io: {
+    enabled: true,
+    config: {
+      IOServerOptions: {
+        cors: { origin: "http://localhost:3000", methods: ["GET", "POST"] },
+      },
+      contentTypes: {
+        message: "*",
+        chat: ["create"],
+      },
+      events: [
+        {
+          name: "connection",
+          handler: ({ strapi }, socket) => {
+            console.log("SOCKET HI");
+
+            strapi.log.info(`[io] new connection with id ${socket.id}`);
+          },
+        },
+      ],
     },
   },
 });
