385711fb5acc96fd66f674906b5d511d50f5f354
Nowik434 <nowik434@gmail.com>
2022-12-21 20:12:17 +0100

    Socket io messages working but i still have some issues with chat rooms

--- src/Pages/Friends/components/ChatRoom/index.js
+++ src/Pages/Friends/components/ChatRoom/index.js
@@ -13,24 +13,24 @@ import {
 import SendIcon from "@mui/icons-material/Send";
 import { socket } from "../../../../config/web-sockets";
 
-function ChatRoom() {
+function ChatRoom({ username, room }) {
   const [messages, setMessages] = useState([
     {
       key: 1,
-      primary: "Hey man, What's up ?",
-      secondary: "09:30",
+      message: "Hey man, What's up ?",
+      createdAt: "09:30",
       align: "right",
     },
     {
       key: 2,
-      primary: "Hey, Iam Good! What about you ?",
-      secondary: "09:31",
+      message: "Hey, Iam Good! What about you ?",
+      createdAt: "09:31",
       align: "left",
     },
     {
       key: 3,
-      primary: "Cool. i am good, let's catch up!",
-      secondary: "09:36",
+      message: "Cool. i am good, let's catch up!",
+      createdAt: "09:36",
       align: "right",
     },
   ]);
@@ -39,54 +39,103 @@ function ChatRoom() {
 
   const navigate = useNavigate();
 
+  // TEST SEND MESSAGES SOCKET IO
+
+  // console.log(messages);
+
+  let welcome;
+
+  useEffect(() => {
+    socket.emit("join", { username: username, room: room }, (error) => {
+      socket.join(room);
+      if (error) return alert(error);
+    });
+    socket.on("welcome", async (data, error) => {
+      console.log("aaa", data);
+      let welcomeMessage = {
+        user: data.user,
+        message: data.text,
+      };
+      welcome = welcomeMessage;
+      setMessages([welcomeMessage]);
+      await fetch("http://localhost:1337/api/messages")
+        .then(async (res) => {
+          const response = await res.json();
+          console.log("aaa", response);
+          let arr = [welcome];
+          response.data.map((one, i) => {
+            arr = [...arr, one.attributes];
+            setMessages((msgs) => arr); // Storing all Messages in a state variable
+          });
+        })
+        .catch((e) => console.log(e.message));
+    });
+    socket.on("message", async (data, error) => {
+      console.log("messagehhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh");
+      console.log("message", data);
+      await fetch("http://localhost:1337/api/messages")
+        .then(async (res) => {
+          const response = await res.json();
+          console.log("aaa", response);
+          let arr = [welcome];
+          response.data.map((one, i) => {
+            arr = [...arr, one.attributes];
+            setMessages((msgs) => arr);
+          });
+        })
+        .catch((e) => console.log(e.message));
+    });
+  }, [username]);
+
+  socket.on("sendMessage", (data) =>
+    console.log("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb", data)
+  );
+  // TEST SEND MESSAGES SOCKET IO
+
   const handleSubmit = (message) => {
-    socket.emit("send-message", { message }, (error, data) => {
-      console.log(data, message);
-    });
-
-    socket.on("connect", () => {
-      socket.on("message:create", (data) => {
-        console.log(data);
-      });
-    });
-
-    console.log(message);
-    console.log([
-      ...messages,
-      {
-        key: messages.length + 1,
-        primary: message,
-        secondary: `${new Date().getHours()}:${new Date().getMinutes()}`,
-        align: "right",
-      },
-    ]);
-    setMessages([
-      ...messages,
-      {
-        key: messages.length + 1,
-        primary: message,
-        secondary: `${new Date().getHours()}:${new Date().getMinutes()}`,
-        align: "right",
-      },
-    ]);
-    setMessage("");
+    if (message) {
+      socket.emit(
+        "sendMessage",
+        { message, user: username, room: room },
+        (error) => {
+          if (error) {
+            alert(error);
+          }
+        }
+      );
+      setMessages([
+        ...messages,
+        {
+          key: messages.length + 1,
+          message: message,
+          createdAt: new Date(),
+          room: room,
+          // createdAt: `${new Date().getHours()}:${new Date().getMinutes()}`,
+          align: "right",
+        },
+      ]);
+      setMessage("");
+    } else {
+      alert("Message can't be empty");
+    }
   };
-
-  socket.on("send-message", { message }, (error, data) => {
-    console.log(data, message);
-  });
 
   return (
     <Grid item xs={9}>
       <List sx={{ height: "70vh", overflowY: "auto" }}>
-        {messages.map((item) => (
-          <ListItem key={item.key}>
+        {messages.map((item, i) => (
+          <ListItem key={i}>
             <Grid container>
               <Grid item xs={12}>
-                <ListItemText align={item.align} primary={item.primary} />
+                <ListItemText align={item.align} primary={item.message} />
               </Grid>
               <Grid item xs={12}>
-                <ListItemText align={item.align} secondary={item.secondary} />
+                <ListItemText
+                  align={item.align}
+                  secondary={`${new Date(item.createdAt).getHours()}:${new Date(
+                    item.createdAt
+                  ).getMinutes()}`}
+                />
               </Grid>
             </Grid>
           </ListItem>
