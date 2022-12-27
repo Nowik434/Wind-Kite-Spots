5dfb23b1f62da35b1552c3731205ad6d2849e741
Nowik434 <nowik434@gmail.com>
2022-12-27 19:27:05 +0100

    update

--- src/Hooks/useDocumentTitle.js
+++ src/Hooks/useDocumentTitle.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
 
-const useDocumentTitle = title => {
+const useDocumentTitle = (title) => {
   const [doctitle, setDocTitle] = useState(title);
 
   useEffect(() => {
-    document.title = `VCC - ${doctitle}`;
+    document.title = `Wind Kite Spots - ${doctitle}`;
   }, [doctitle]);
 
   return [doctitle, setDocTitle];
 };
 
-export {useDocumentTitle};
+export { useDocumentTitle };
